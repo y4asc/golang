fd65b296015dca96ac8fb2476bce534d2d2ac7e1
y4asc <yasmim.candido@aluno.senai.br>
2025-03-28 09:30:01 -0300

    senha e usuario

--- main.go
+++ main.go
@@ -3,16 +3,21 @@ package main
 import "fmt" 
 
 func main(){
-	var n1 float32
-	var n2 float32
-fmt.Println("digite dois numeros")
-fmt.Scan(&n1 , &n2)
-fmt.Println("o numero digitado é:", n1 , n2)
-	fmt.Println("a soma é:", n1 + n2)
-	fmt.Println("a subtração é:", n1 - n2)
-	fmt.Println("a divisão é:", n1 / n2)
-	fmt.Println("a multiplicação é:", n1 * n2)
+	var usuario string
+	var senha string
+fmt.Println("digite seu usuário")
+fmt.Scan(&usuario)
+ if usuario == "admin"{
+	fmt.Println("digite sua senha")
+	fmt.Scan(&senha)
+	if senha == "1234"{
+		fmt.Println("senha correta")
+	}else{
+		fmt.Println("senha incorreta, acesso negado! tente novamente")
+	}
 
-
+ } else {
+	fmt.Println("usuário incorreto, tente novamente")
+ }
 	
 } 
