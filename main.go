e3c4bbf5ad04d65f7e42cd717a115c1429980a94
y4asc <yasmim.candido@aluno.senai.br>
2025-03-28 08:54:43 -0300

    calculando

--- main.go
+++ main.go
@@ -3,16 +3,16 @@ package main
 import "fmt" 
 
 func main(){
-	a, b := 10, 3
-	fmt.Println("a soma é:", a + b)
-	fmt.Println("a subtração é:", a - b)
-	fmt.Println("a divisão é:", a / b)
-	fmt.Println("a multiplicação é:", a * b)
-	fmt.Println("o resto da divisão:", a % b)
+	var n1 float32
+	var n2 float32
+fmt.Println("digite dois numeros")
+fmt.Scan(&n1 , &n2)
+fmt.Println("o numero digitado é:", n1 , n2)
+	fmt.Println("a soma é:", n1 + n2)
+	fmt.Println("a subtração é:", n1 - n2)
+	fmt.Println("a divisão é:", n1 / n2)
+	fmt.Println("a multiplicação é:", n1 * n2)
 
-	a+=1
-	fmt.Println("incrementa a", a)
 
-	if a > 0 && b > 0 {
-		fmt.Println("numeros positivos")
-} }
+	
+} 
