25d02512232c520209fef7f147a354e9354750cf
y4asc <yasmim.candido@aluno.senai.br>
2025-04-03 09:19:14 -0300

    slice

--- main.go
+++ main.go
@@ -9,5 +9,21 @@ func main(){
 	fmt.Println(nomes)
 	nomes[3] = "clarkman"
 	fmt.Println(nomes)
-	
-} 
+	// Slice
+	var score = []int{100, 200, 300, 400}
+	fmt.Println(score)
+	score[1] = 2 
+	fmt.Println(score, len(score), cap(score))
+	rangeOne := score[1:3]
+	fmt.Println(rangeOne)
+	rangeTwo := score[2:]
+	fmt.Println(rangeTwo)
+	rangeThree := score[:3]
+	fmt.Println(rangeThree)
+
+	var superherois = []string{"deadPooll", "homem-aranha", "motoqueiro fantasma"}
+	fmt.Println(superherois)
+	superherois = append(superherois, "ben 10", "ciborg", "lavagril", "sharkboy")
+fmt.Println(superherois, len(superherois), cap(superherois))
+
+}
